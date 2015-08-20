10d68ce3ebe3484404c5262cb41e6113e50a0da0
madobon <mimicopy@live.jp>
2015-08-20 22:53:46 +0900

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# knowledge documentation build configuration file, created by
+# Wiki documentation build configuration file, created by
 # sphinx-quickstart on Sat Apr 12 12:22:20 2014.
 #
 # This file is execfile()d with the current directory set to its
@@ -45,7 +45,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'knowledge'
+project = u'Wiki'
 copyright = u'2014, madobon'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -178,7 +178,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'knowledgedoc'
+htmlhelp_basename = 'Wikidoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -198,7 +198,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'knowledge.tex', u'knowledge Documentation',
+  ('index', 'Wiki.tex', u'Wiki Documentation',
    u'madobon', 'manual'),
 ]
 
@@ -228,7 +228,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'knowledge', u'knowledge Documentation',
+    ('index', 'Wiki', u'Wiki Documentation',
      [u'madobon'], 1)
 ]
 
@@ -242,8 +242,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'knowledge', u'knowledge Documentation',
-   u'madobon', 'knowledge', 'One line description of project.',
+  ('index', 'Wiki', u'Wiki Documentation',
+   u'madobon', 'Wiki', 'One line description of project.',
    'Miscellaneous'),
 ]
 
